fea99223df829bc48c6368ae6164f4f6bd95d068
Nike Ojetola <ojetolanike@gmail.com>
2022-06-30 16:32:25 -0400

    made some api routes

---
+++ server/api/exercises.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  models: { Exercise },
+} = require('../db');
+const sequelize = require('sequelize');
+module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+    const exercises = await Exercise.findAll();
+    res.json(exercises);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const exercise = await Exercise.findByPk(id);
+    res.json(exercise);
+  } catch (error) {
+    next(error);
+  }
+});
