d851a15bb94263b6fa1721d446071317345fb146
Nathan Needham <65629528+nneedham@users.noreply.github.com>
2022-06-30 12:21:14 -0700

    add navigation functionality

--- App.js
+++ App.js
@@ -4,9 +4,21 @@ import {
   ApplicationProvider,
 } from '@ui-kitten/components';
 import HomeScreen from './components/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
+  );
+}
+
+export default App;
