5511077edbab21ae721f638446033c86e4fd5fb2
kwangric <43795498+kwangric@users.noreply.github.com>
2022-06-30 17:11:38 -0400

    Created Routine and Session API

---
+++ server/api/routine.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const {
+  models: {Routine, Exercise}
+} = require('../db')
+module.exports = router
+
+// api/routine/
+// GET ALL ROUTINES
+router.get('/', async(req, res, next) => {
+  try {
+    const routines = await Routine.findAll({include: [Exercise]})
+    res.json(routines)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// api/routine/:id
+// GET SPECIFIC ROUTINE BY ROUTINEID
+router.get('/:id', async(req, res, next) => {
+  try {
+    const routineId = req.params.id
+    const routine = await Routine.findByPk(routineId, {include: [Exercise]})
+    res.json(routine)
+  } catch (err) {
+    next(err)
+  }
+})
---
+++ server/api/session.js
@@ -0,0 +1,102 @@
+const router = require('express').Router()
+const {
+  models: { Session, Routine, Exercise, User, SessionExercise },
+} = require('../db')
+module.exports = router
+
+// api/session/
+// GET ALL SESSIONS
+router.get('/', async (req, res, next) => {
+  try {
+    const sessions = await Session.findAll({
+      include: [Routine, SessionExercise],
+    })
+    res.json(sessions)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// api/session/all/:userId
+// GET ALL SESSION FROM USER
+router.get('/all/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const sessions = await Session.findAll({
+      where: {
+        userId: userId,
+      },
+    })
+    res.json(sessions)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// api/session/current/:userId
+// GET CURRENT SESSION FROM USER
+router.get('/current/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const session = await Session.findOne({
+      where: {
+        userId: userId,
+        complete: false,
+      },
+    })
+    res.json(session)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// api/session/new/:userId
+// CREATE NEW SESSION FOR USER
+// req.body needs date and routineId (example: {date: 2022-06-29, routineId: 2)
+router.post('/start/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const check = await Session.findOne({
+      where: {
+        userId: userId,
+        complete: false,
+      },
+    })
+    // Check if there is a current session for user. If so, return session
+    if (check) {
+      res.json(check)
+    } else {
+      // If no current session for user, create session along with session exercises, then returns it
+      const user = await User.findByPk(userId)
+      const date = req.body.date
+      const routineId = req.body.routineId
+      const session = await Session.create({date, routineId})
+      const routine = await Routine.findByPk(routineId, {include: [Exercise]})
+      for (let i = 0; i < routine.exercises.length; i++) {
+        await SessionExercise.create({exerciseId: routine.exercises[i].id, userId, sessionId: session.id})
+      }
+      await user.addSession(session)
+      res.json(session)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+// api/session/complete/:userId
+// COMPLETE CURRENT SESSION FOR USER
+router.put('/complete/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const session = await Session.findOne({
+      where: {
+        userId: userId,
+        complete: false,
+      },
+    })
+    await session.update({ complete: true })
+    res.json(session)
+  } catch (err) {
+    next(err)
+  }
+})
