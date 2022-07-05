ad4a7af4948e8bb598922e589768035ee11bb18f
kwangric <43795498+kwangric@users.noreply.github.com>
2022-07-05 16:54:09 -0400

    Made changes to session exercise api to include more information

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -26,6 +26,7 @@ router.get('/all/:userId', async (req, res, next) => {
       where: {
         userId: userId,
       },
+      include: [Routine, SessionExercise],
     })
     res.json(sessions)
   } catch (err) {
@@ -44,7 +45,11 @@ router.get('/current/:userId', async (req, res, next) => {
         complete: false,
       },
     })
-    res.json(session)
+    if (!session) {
+      res.send("User has no active session!")
+    } else {
+      res.json(session)
+    }
   } catch (err) {
     next(err)
   }
@@ -70,10 +75,14 @@ router.post('/start/:userId', async (req, res, next) => {
       const user = await User.findByPk(userId)
       const date = req.body.date
       const routineId = req.body.routineId
-      const session = await Session.create({date, routineId})
-      const routine = await Routine.findByPk(routineId, {include: [Exercise]})
+      const session = await Session.create({ date, routineId })
+      const routine = await Routine.findByPk(routineId, { include: [Exercise] })
       for (let i = 0; i < routine.exercises.length; i++) {
-        await SessionExercise.create({exerciseId: routine.exercises[i].id, userId, sessionId: session.id})
+        await SessionExercise.create({
+          exerciseId: routine.exercises[i].id,
+          userId,
+          sessionId: session.id,
+        })
       }
       await user.addSession(session)
       res.json(session)
