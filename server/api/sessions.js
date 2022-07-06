1c09ffb77bd930c46d18f5f1577e8b7bf3e03192
kwangric <43795498+kwangric@users.noreply.github.com>
2022-07-06 13:59:16 -0400

    Changes to session api

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -68,7 +68,7 @@ router.get('/current/:userId', async (req, res, next) => {
 })
 
 // api/sessions/start/:userId
-// CREATE NEW SESSION FOR USER (NO REQ.BODY REQUIRED)
+// CREATE NEW SESSION FOR USER
 // req.body needs date and routineId (example: {date: 2022-06-29, routineId: 2)
 router.post('/start/:userId', async (req, res, next) => {
   try {
