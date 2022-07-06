e58901f87fa821ad7286d4cabb01aed29f26722e
Nike Ojetola <ojetolanike@gmail.com>
2022-07-06 18:00:48 -0400

    user screen

--- Client/context/AuthContext.js
+++ Client/context/AuthContext.js
@@ -13,7 +13,7 @@ export const AuthProvider = ({ children }) => {
     setIsLoading(true);
 
     const { data } = await axios.post(
-      'http://192.168.0.102:8080/api/auth/login',
+      'https://fitquestapp.herokuapp.com/api/auth/login',
       { email, password }
     );
     let foundUser = data;
@@ -23,9 +23,12 @@ export const AuthProvider = ({ children }) => {
     AsyncStorage.setItem('userToken', foundUser.token);
     // console.log(data.token);
     axios.defaults.headers.common['Authorization'] = foundUser.token;
-    const user = await axios.get('http://192.168.0.102:8080/api/auth/me', {
-      token: data.token,
-    });
+    const user = await axios.get(
+      'https://fitquestapp.herokuapp.com/api/auth/me',
+      {
+        token: data.token,
+      }
+    );
 
     setIsLoading(false);
   };
@@ -33,18 +36,23 @@ export const AuthProvider = ({ children }) => {
   const signUp = async (email, password, firstName, lastName) => {
     setIsLoading(true);
     const { data } = await axios.post(
-      'http://192.168.0.102:8080/api/auth/signup',
+      'https://fitquestapp.herokuapp.com/api/auth/signup',
       { email, password, firstName, lastName }
     );
     let foundUser = data;
     console.log('data :>> ', data);
+    setUserInfo(foundUser);
+    setUserToken(foundUser.token);
     AsyncStorage.setItem('userInfo', JSON.stringify(foundUser));
     AsyncStorage.setItem('userToken', foundUser.token);
     axios.defaults.headers.common['Authorization'] = foundUser.token;
-    const user = await axios.get('http://192.168.0.102:8080/api/auth/me', {
-      token: data.token,
-    });
- 
+    const user = await axios.get(
+      'https://fitquestapp.herokuapp.com/api/auth/me',
+      {
+        token: data.token,
+      }
+    );
+
     //TODO User needs to signup and then only be able to see the logged in user screens. Seems like it's not reading the token. Also needs to handle if
     //someone tries to signup with the same email address.
     setIsLoading(false);
--- Client/navigation/AuthUser.js
+++ Client/navigation/AuthUser.js
@@ -1,22 +1,100 @@
 import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  Modal,
+  Image,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
 import { AuthContext } from '../context/AuthContext';
+import { ProgressBar, MD3Colors } from 'react-native-paper';
 
+const image = {
+  uri: 'https://images.pexels.com/photos/775201/pexels-photo-775201.jpeg?cs=srgb&dl=pexels-kaique-rocha-775201.jpg&fm=jpg',
+};
 const AuthUser = () => {
   const { logout } = useContext(AuthContext);
   // console.log('this is props', props);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello</Text>
-      <Button
-        title="logout"
-        style={{ margin: 10 }}
-        onPress={() => {
-          logout();
-        }}
-      ></Button>
-    </View>
+    <Modal animationType="slide">
+      <View style={styles.container}>
+        <ImageBackground
+          source={image}
+          resizeMode="cover"
+          style={styles.backgroundImage}
+        >
+          <View style={styles.image}>
+            <Image source={require('../../src/assets/favicon.png')} />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text>Username</Text>
+            <Text>Class</Text>
+            <Text>Level 99</Text>
+            <ProgressBar progress={0.5} />
+            <View style={styles.button}>
+              <Button color="black" title="Start workout" />
+            </View>
+            <View style={styles.button}>
+              <Button color="black" title="History" />
+            </View>
+            <View style={styles.button}>
+              <Button color="black" title="Character" />
+            </View>
+
+            <View>
+              <Button
+                title="logout"
+                style={{ margin: 10 }}
+                onPress={() => {
+                  logout();
+                }}
+              ></Button>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 60,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    opacity: 0.8,
+  },
+
+  button: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    backgroundColor: 'grey',
+    borderRadius: 20,
+    width: '60%',
+    marginRight: 8,
+    marginTop: 8,
+    padding: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textDecorationLine: 'underline',
+  },
+});
 
 export default AuthUser;
