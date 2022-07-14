dc2060ab0d44f3570b3640e354c14f9481cc502e
Nike Ojetola <ojetolanike@gmail.com>
2022-07-14 17:03:30 -0400

    made visuals

--- Client/components/SessionExercise.js
+++ Client/components/SessionExercise.js
@@ -6,7 +6,6 @@ import {
   StyleSheet,
   ImageBackground,
   ScrollView,
-  TouchableOpacity,
   TextInput,
   Image,
   Linking,
@@ -69,21 +68,15 @@ const SessionExercise = ({ navigation }) => {
               <TextInput
                 onChangeText={(nextValue) => setWeight1(nextValue)}
                 style={styles.textInput}
-              >
-                {sessionExercise.weight1}
-              </TextInput>
+              ></TextInput>
               <TextInput
                 onChangeText={(nextValue) => setWeight2(nextValue)}
                 style={styles.textInput}
-              >
-                {sessionExercise.weight2}
-              </TextInput>
+              ></TextInput>
               <TextInput
                 onChangeText={(nextValue) => setWeight3(nextValue)}
                 style={styles.textInput}
-              >
-                {sessionExercise.weight3}
-              </TextInput>
+              ></TextInput>
             </View>
 
             <View style={{ alignItems: 'center', padding: 2 }}>
@@ -91,21 +84,15 @@ const SessionExercise = ({ navigation }) => {
               <TextInput
                 onChangeText={(nextValue) => setSet1(nextValue)}
                 style={styles.textInput}
-              >
-                {sessionExercise.set1}
-              </TextInput>
+              ></TextInput>
               <TextInput
                 onChangeText={(nextValue) => setSet2(nextValue)}
                 style={styles.textInput}
-              >
-                {sessionExercise.set2}
-              </TextInput>
+              ></TextInput>
               <TextInput
                 onChangeText={(nextValue) => setSet3(nextValue)}
                 style={styles.textInput}
-              >
-                {sessionExercise.set3}
-              </TextInput>
+              ></TextInput>
             </View>
           </View>
 
@@ -135,7 +122,13 @@ const SessionExercise = ({ navigation }) => {
                 setDescription('history');
               }}
             />
-            <Button title="Charts" color="black" />
+            <Button
+              title="Visual"
+              color="black"
+              onPress={() => {
+                setDescription('visual');
+              }}
+            />
           </View>
         </View>
 
@@ -145,23 +138,31 @@ const SessionExercise = ({ navigation }) => {
               <Text style={{ textAlign: 'center' }}>
                 {sessionExercise.exercise.description}
               </Text>
-
-              <View style={styles.imageContainer}>
-                <Image
-                  source={{ uri: sessionExercise.exercise.image }}
-                  style={{ width: '100%', height: '100%' }}
-                />
-              </View>
-
-              <Text
-                style={{ color: 'blue', textAlign: 'center' }}
-                onPress={() => Linking.openURL(sessionExercise.exercise.video)}
-              >
-                Checkout video
-              </Text>
             </ScrollView>
           ) : (
-            <ExerciseHistory exerciseId={sessionExercise.exercise.id} />
+            <>
+              {description === 'visual' ? (
+                <>
+                  <View style={styles.imageContainer}>
+                    <Image
+                      source={{ uri: sessionExercise.exercise.image }}
+                      style={{ width: '100%', height: '100%' }}
+                    />
+                  </View>
+
+                  <Text
+                    style={{ color: 'blue', textAlign: 'center' }}
+                    onPress={() =>
+                      Linking.openURL(sessionExercise.exercise.video)
+                    }
+                  >
+                    Checkout video
+                  </Text>
+                </>
+              ) : (
+                <ExerciseHistory exerciseId={sessionExercise.exercise.id} />
+              )}
+            </>
           )}
         </View>
       </ImageBackground>
@@ -169,7 +170,7 @@ const SessionExercise = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -182,7 +183,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
-    margin: 35,
+    margin: 50,
     opacity: 0.8,
     borderRadius: 5,
   },
@@ -205,7 +206,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#dddddd',
     opacity: 0.9,
     borderRadius: 10,
-    margin: 20,
+    margin: 10,
     padding: 10,
   },
 
@@ -226,7 +227,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#dddddd',
     opacity: 0.9,
     margin: 20,
-    padding: 10,
+    padding: 40,
     borderRadius: 5,
   },
   navigationContainer: {
@@ -259,6 +260,7 @@ const styles = StyleSheet.create({
     padding: 5,
     borderRadius: 5,
     margin: 5,
+    width: 50,
   },
   navButton: {
     backgroundColor: 'white',
