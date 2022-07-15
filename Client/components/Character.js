54338d853df081ff719bdae9f2074457065e33ab
Nathan Needham <needham.nathan@gmail.com>
2022-07-15 12:17:09 -0700

    stashed character page

--- Client/components/Character.js
+++ Client/components/Character.js
@@ -3,13 +3,16 @@ import {
   View,
   Text,
   Button,
+  Modal,
   Image,
   StyleSheet,
   ImageBackground,
+  Alert,
   ScrollView,
 } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
 import { Bar } from 'react-native-progress';
+import * as Font from 'expo-font';
 
 const Character = ({ navigation }) => {
   const { user, getSession, userItems, getUserItems, updateUserItems } =
@@ -43,12 +46,28 @@ const Character = ({ navigation }) => {
   const [legsLevelBonus, setLegsLevelBonus] = useState(0);
   const [shouldersLevelBonus, setShouldersLevelBonus] = useState(0);
   const [stretchingLevelBonus, setStretchingLevelBonus] = useState(0);
+  const [selectedItem, setSelectedItem] = useState(-1);
+  const [selectedItemIsEquipped, setSelectedItemIsEquipped] = useState(false);
 
   const showInventoryFunc = () => {
     if (showInventory) {
       setShowInventory(false);
     } else {
       setShowInventory(true);
+    }
+  };
+
+  const isSelectedItemEquipped = (item) => {
+    if (
+      headItem === item ||
+      chestItem === item ||
+      legItem === item ||
+      ringItem === item ||
+      weaponItem === item
+    ) {
+      setSelectedItemIsEquipped(true);
+    } else {
+      setSelectedItemIsEquipped(false);
     }
   };
 
@@ -77,6 +96,9 @@ const Character = ({ navigation }) => {
   }
 
   //grab items if user has items
+  useEffect(() => {
+    getUserItems(user.id);
+  }, []);
 
   // let currentLevelString = user.currentLevel.toString();
   // console.log(getSession);
@@ -84,440 +106,33 @@ const Character = ({ navigation }) => {
   // console.log(levelExp[currentLevelString]);
   // console.log(userItems);
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('../../src/assets/background.jpeg')}
-        resizeMode="cover"
-        imageStyle={{ opacity: 0.9 }}
-        style={styles.backgroundImage}
-      >
-        <View style={styles.main}>
-          <View style={styles.top}>
-            <View style={styles.container}>
-              <Image
-                source={{
-                  uri: 'https://i.imgur.com/4Ef2wUE.png',
-                }}
-                style={{ width: 100, height: 100 }}
-              />
-            </View>
-            <View style={styles.topTextBox}>
-              <Text>{user.name}</Text>
-              <Text>Level: {user.currentLevel}</Text>
-              <Bar
-                progress={
-                  user.currentLevelExp /
-                  levelExp[(user.currentLevel + 1).toString()]
-                }
-                color={'red'}
-                height={15}
-              />
-              <Text>
-                {user.currentLevelExp} /{' '}
-                {levelExp[(user.currentLevel + 1).toString()]} XP
-              </Text>
-              <Text>Coins: ${user.coins}</Text>
-              <Text>Combat Skill: {combatSkill}</Text>
-            </View>
-          </View>
-          <View style={styles.allSkillsContainer}>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Abdominals</Text>
-              {abdominalsLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {abdominalsLevel + abdominalsLevelBonus}
-
-              )}
-              <Bar
-                progress={
-                  user.abdominalsCurrentLevelExp /
-                  levelExp[(user.abdominalsCurrentLevel + 1).toString()]
-                }
-                color={'red'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Arms</Text>
-              {armsLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {armsLevel + armsLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {armsLevel + armsLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.armsCurrentLevelExp /
-                  levelExp[(user.armsCurrentLevel + 1).toString()]
-                }
-                color={'orange'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Back</Text>
-              {backLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {backLevel + backLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {backLevel + backLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.backCurrentLevelExp /
-                  levelExp[(user.backCurrentLevel + 1).toString()]
-                }
-                color={'yellow'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Cardio</Text>
-              {cardioLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {cardioLevel + cardioLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {cardioLevel + cardioLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.cardioCurrentLevelExp /
-                  levelExp[(user.cardioCurrentLevel + 1).toString()]
-                }
-                color={'lime'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Chest</Text>
-              {chestLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {chestLevel + chestLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {chestLevel + chestLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.chestCurrentLevelExp /
-                  levelExp[(user.chestCurrentLevel + 1).toString()]
-                }
-                color={'green'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Legs</Text>
-              {legsLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {legsLevel + legsLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {legsLevel + legsLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.legsCurrentLevelExp /
-                  levelExp[(user.legsCurrentLevel + 1).toString()]
-                }
-                color={'blue'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Shoulders</Text>
-              {shouldersLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {shouldersLevel + shouldersLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {shouldersLevel + shouldersLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.shouldersCurrentLevelExp /
-                  levelExp[(user.shouldersCurrentLevel + 1).toString()]
-                }
-                color={'purple'}
-                width={150}
-                height={15}
-              />
-            </View>
-            <View style={styles.skillsContainer}>
-              <Text style={styles.skillsTitle}>Stretching</Text>
-              {stretchingLevelBonus > 0 ? (
-                <Text style={styles.skillsLevelBonus}>
-                  Level {stretchingLevel + stretchingLevelBonus}
-                </Text>
-              ) : (
-                <Text style={styles.skillsLevel}>
-                  Level {stretchingLevel + stretchingLevelBonus}
-                </Text>
-              )}
-              <Bar
-                progress={
-                  user.stretchingCurrentLevelExp /
-                  levelExp[(user.stretchingCurrentLevel + 1).toString()]
-                }
-                color={'pink'}
-                width={150}
-                height={15}
-              />
-            </View>
-          </View>
-          <View style={styles.allItemsContainer}>
-            <View style={styles.singleItemContainer}>
-              {headItem ? (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        setHeadItem(null);
-                      }}
-                    >
-                      {headItem.name}
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Head</Text>
-                  </View>
-                </View>
-              ) : (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        showInventoryFunc();
-                      }}
-                    >
-                      Equip
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Head</Text>
-                  </View>
-                </View>
-              )}
-            </View>
-            <View style={styles.singleItemContainer}>
-              {chestItem ? (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        setChestItem(null);
-                      }}
-                    >
-                      {chestItem.name}
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Chest</Text>
-                  </View>
-                </View>
-              ) : (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        showInventoryFunc();
-                      }}
-                    >
-                      Equip
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Chest</Text>
-                  </View>
-                </View>
-              )}
-            </View>
-            <View style={styles.singleItemContainer}>
-              {legItem ? (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        setLegItem(null);
-                      }}
-                    >
-                      {legItem.name}
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Leg</Text>
-                  </View>
-                </View>
-              ) : (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        showInventoryFunc();
-                      }}
-                    >
-                      Equip
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Legs</Text>
-                  </View>
-                </View>
-              )}
-
-            </View>
-            <View style={styles.singleItemContainer}>
-              {weaponItem ? (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        setWeaponItem(null);
-                      }}
-                    >
-                      {weaponItem.name}
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Weapon</Text>
-                  </View>
-                </View>
-
-              ) : (
-                <View>
-                  <View style={styles.emptyInventorySlot}>
-                    <Text
-                      style={styles.emptyInventoryText}
-                      onPress={() => {
-                        showInventoryFunc();
-                      }}
-                    >
-                      Equip
-                    </Text>
-                  </View>
-                  <View style={styles.intentoryTextTitleContainer}>
-                    <Text>Weapon</Text>
-                  </View>
-                </View>
-              )}
-            </View>
-          </View>
-          {showInventory ? (
-            <View style={styles.allItemsContainer}>
-              {userItems.map((item) => {
-                if (
-                  headItem === item ||
-                  chestItem === item ||
-                  legItem === item ||
-                  ringItem === item ||
-                  weaponItem === item
-                ) {
-                  return <View key={item.id}></View>;
-                } else {
-                  return (
-                    <View key={item.id} style={styles.singleItemContainer}>
-                      <View style={styles.emptyInventorySlot}>
-                        <Text
-                          style={styles.emptyInventoryText}
-                          onPress={() => {
-                            if (item.type === 'head') {
-                              setHeadItem(item);
-                            } else if (item.type === 'chest') {
-                              setChestItem(item);
-                            } else if (item.type === 'leg') {
-                              setLegItem(item);
-                            } else if (item.type === 'ring') {
-                              setRingItem(item);
-                            } else if (item.type === 'weapon') {
-                              setWeaponItem(item);
-                            }
-                            updateUserItems(user.id, item.id);
-                          }}
-                        >
-                          {item.name}
-                        </Text>
-                      </View>
-                      <View style={styles.intentoryTextTitleContainer}>
-                        <Text>{item.type}</Text>
-                      </View>
-                    </View>
-                  );
-                }
-              })}
-            </View>
-          ) : (
-            <View></View>
-          )}
-          {!showInventory ? (
-            <View style={styles.button}>
-              <ScrollView>
-                <Button
-                  color="black"
-                  title="Inventory"
-                  onPress={() => {
-                    showInventoryFunc();
-                  }}
-                />
-              </ScrollView>
-            </View>
-          ) : (
-            <View></View>
-          )}
-        </View>
-      </ImageBackground>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('../../src/assets/background.jpeg')}
+          resizeMode="cover"
+          style={styles.backgroundImage}
+        ></ImageBackground>
+      </View>
+    </ScrollView>
   );
 };
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
   },
   backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   main: {
-    flex: 1,
-    backgroundColor: 'white',
-    margin: 10,
-    marginTop: 50,
-
-    opacity: 0.8,
-
-    borderRadius: 10,
+    flexDirection: 'column',
+    paddingTop: 35,
   },
   top: {
     flexDirection: 'row',
+    backgroundColor: 'white',
     borderRadius: 20,
     opacity: 0.8,
     margin: 10,
@@ -530,6 +145,8 @@ export const styles = StyleSheet.create({
     paddingLeft: 20,
   },
   allSkillsContainer: {
+    backgroundColor: 'white',
+
     borderRadius: 20,
     opacity: 0.8,
     margin: 10,
@@ -550,13 +167,13 @@ export const styles = StyleSheet.create({
     width: 60,
     color: 'red',
   },
-
   allItemsContainer: {
     flexDirection: 'row',
     maxWidth: 400,
     flex: 1,
     flexWrap: 'wrap',
-    borderRadius: 10,
+    backgroundColor: 'white',
+    borderRadius: 20,
     opacity: 0.8,
     margin: 10,
     padding: 10,
@@ -564,6 +181,7 @@ export const styles = StyleSheet.create({
   singleItemContainer: {
     flex: 1,
     alignItems: 'center',
+    // justifyContent: 'center',
     margin: 5,
     padding: 5,
     minWidth: 70,
@@ -571,13 +189,23 @@ export const styles = StyleSheet.create({
   emptyInventorySlot: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#AAAAAA',
-    borderRadius: 10,
+    backgroundColor: 'grey',
+    borderRadius: 20,
     height: 70,
     width: 70,
   },
+  selectedInventorySlot: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+    borderRadius: 20,
+    height: 70,
+    width: 70,
+    borderColor: 'red',
+    borderWidth: 3,
+  },
   emptyInventoryText: {
-    color: 'black',
+    color: 'white',
   },
   intentoryTextTitleContainer: {
     alignItems: 'center',
@@ -599,7 +227,8 @@ export const styles = StyleSheet.create({
     borderColor: '#3D3D3D',
     backgroundColor: 'grey',
     borderRadius: 20,
-    margin: 60,
+    width: '90%',
+    margin: 8,
     marginTop: 8,
     padding: 3,
     justifyContent: 'center',
@@ -612,6 +241,29 @@ export const styles = StyleSheet.create({
     opacity: 0.8,
     margin: 10,
     padding: 10,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 35,
+    width: '100%',
+    flexDirection: 'row',
+  },
+  allItemsContainerModal: {
+    flexDirection: 'row',
+    maxWidth: 400,
+    flex: 1,
+    flexWrap: 'wrap',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    opacity: 0.8,
+    margin: 10,
+    padding: 10,
+    maxWidth: 200,
+  },
+  topContainerModal: {
+    flexDirection: 'row',
   },
 });
 
